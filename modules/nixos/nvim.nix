537cdaa9f124861bebb8e1fe80fde9f75eae871b
alexborrazasm <alexborrazasm@gmail.com>
2025-03-02 00:57:36 +0100

    ammend last 2 commits

--- modules/nixos/nvim.nix
+++ modules/nixos/nvim.nix
@@ -10,32 +10,31 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs.neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-      extraConfig = ''
-        augroup cursor_reset
-          autocmd!
-          autocmd VimLeave * set guicursor=a:ver25
-        augroup END
-
-        " Use spaces
-        set expandtab
-        " 4 spaces
-        set tabstop=4
-        set shiftwidth=4
-        set softtabstop=4
-
-        syntax on
-
-        set number
-        set relativenumber
-      '';
-    };
-
     environment.systemPackages = with pkgs; [
-      neovim
+      (neovim.override {
+        configure = {
+          customRC = ''
+            augroup cursor_reset
+              autocmd!
+              autocmd VimLeave * set guicursor=a:ver25
+            augroup END
+
+            " Use spaces
+            set expandtab
+            " 4 spaces
+            set tabstop=4
+            set shiftwidth=4
+            set softtabstop=4
+
+            syntax on
+
+            set number
+            set relativenumber
+          '';
+        };
+      })
     ];
+
+    environment.variables.EDITOR = "nvim";
   };
 }
