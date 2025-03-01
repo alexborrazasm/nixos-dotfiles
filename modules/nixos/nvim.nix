b8d1257730f3852a46e51789287549ab983f6f6e
alexborrazasm <alexborrazasm@gmail.com>
2025-03-02 00:58:07 +0100

    change tabspaces nvim

--- modules/nixos/nvim.nix
+++ modules/nixos/nvim.nix
@@ -21,10 +21,10 @@ in {
 
             " Use spaces
             set expandtab
-            " 4 spaces
-            set tabstop=4
-            set shiftwidth=4
-            set softtabstop=4
+            " 2 spaces
+            set tabstop=2
+            set shiftwidth=2
+            set softtabstop=2
 
             syntax on
 
