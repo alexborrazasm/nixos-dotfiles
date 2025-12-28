75d660dc8aff296c7189f82cdbd6c92509b8ad37
alexborrazasm <alexborrazasm@gmail.com>
2025-12-28 04:17:47 +0100

    Added grub2 theme

--- hosts/zen/default.nix
+++ hosts/zen/default.nix
@@ -18,16 +18,29 @@
     loader = {
       systemd-boot.enable = false;  # Disable systemd-boot
       efi.canTouchEfiVariables = true;
-      
+
       grub = {
         enable = true;
         device = "nodev";
         useOSProber = true;
         efiSupport = true;
+	gfxmodeEfi = "2880x1800,auto";
+        splashImage = null;
         default = "saved";
         extraConfig = ''
           set save_default=true
         '';
+	theme = pkgs.stdenv.mkDerivation {
+          pname = "distro-grub-themes";
+          version = "3.1";
+          src = pkgs.fetchFromGitHub {
+            owner = "AdisonCavani";
+            repo = "distro-grub-themes";
+            rev = "v3.1";
+            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+          };
+          installPhase = "cp -r customize/nixos $out";
+        };
       };
     };
 
